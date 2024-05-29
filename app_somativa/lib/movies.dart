1ab5ca57ebfd834f0a56f64bc14bdf6c83bfd585
Vinicius-Maia04 <vinimaia200@gmail.com>
2024-05-28 22:59:19 -0300

    Atualização Somativa
    Tentando puxar os filmes

--- app_somativa/lib/movies.dart
+++ app_somativa/lib/movies.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
 
 class Movies extends StatefulWidget {
   const Movies({super.key});
@@ -8,9 +12,35 @@ class Movies extends StatefulWidget {
 }
 
 class _MoviesState extends State<Movies> {
-
+  String movies_url = 'https://raw.githubusercontent.com/danielvieira95/DESM-2/master/filmes.json';
+  List data = [];
   _getMovies()async{
+    http.Response response = await http.get(Uri.parse(movies_url));
+    data = json.decode(response.body) as List;
     
+    for(int i=0; i<data.length;){
+      MovieInformation(BuildContext context){
+        SizedBox(
+          width: MediaQuery.of(context).size.width/1.9,
+          height: MediaQuery.of(context).size.height/4,
+          child: Row(
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width/2,
+                height: MediaQuery.of(context).size.height/1.1,
+                child: Image.network(data[i]["imagem"]),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width/2,
+                height: MediaQuery.of(context).size.height/1.1,
+              )
+            ],
+          ),
+        );
+      }
+      i++;
+      return MovieInformation(context);
+    }
   }
 
   @override
@@ -23,7 +53,6 @@ class _MoviesState extends State<Movies> {
       ),
       body: ListView(
         children: [
-
         ],
       ),
     );
